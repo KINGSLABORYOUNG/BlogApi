5ec6499e9f31dd55a932f0fb90b2eccfbce05e81
KINGSLABORYOUNG <kingdomucheoma@gmail.com>
2025-01-30 13:08:52 +0100

    blogapi

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k)+*suc$*@&)n+*&_03*kyc9rzf+kknpvksg43f6yiap5@j9!x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
